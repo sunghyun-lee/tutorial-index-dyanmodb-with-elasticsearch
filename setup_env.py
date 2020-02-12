9d3d736c8c5f88e6da7b04707308dd69a68468c1
sunghyunlee <sunghyunlee@mymusictaste.com>
2020-02-12 18:49:07 +0900

    feat: add main runner

---
+++ setup_env.py
@@ -0,0 +1,9 @@
+import utils
+
+
+if __name__ == "__main__":
+    stream_arn = utils.prepare_dynamodb_table()
+    function_arn = utils.create_lambda_function()
+    utils.create_elasticsearch_domain()
+    utils.create_event_source_mapping(stream_arn)
+    utils.create_item_on_dynamodb()
